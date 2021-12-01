a77fcc187c1b346329ab85d19836b03876e911a6
isaac <isaacgallardo@iesflorenciopintado.es>
2021-12-01 16:32:25 +0100

    Segunda Parte con Random de Preguntas

--- app/src/main/java/com/example/hagotestymemuevounmontn/MenuPrincipal.java
+++ app/src/main/java/com/example/hagotestymemuevounmontn/MenuPrincipal.java
@@ -8,17 +8,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class MenuPrincipal extends AppCompatActivity {
-    Button buttonPreparar;
+    Button buttonPreparar, buttonJugar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
         buttonPreparar=findViewById(R.id.buttonPreparar);
+        buttonJugar=findViewById(R.id.buttonJugar);
 
         buttonPreparar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MenuPrincipal.this,PrepararPreguntas.class);
+                startActivity(intent);
+            }
+        });
+        buttonJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipal.this,JugarPreguntas.class);
                 startActivity(intent);
             }
         });
